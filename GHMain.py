40786a0bbf61fa00382b946f27c81be08f193903
Tybalta <Tybalta@users.noreply.github.com>
2020-03-04 14:29:35 +0000

    Added Class files. Genotype generation added. Genotype to Phenotype Added. Grammar File made but empty.

--- GHMain.py
+++ GHMain.py
@@ -7,15 +7,13 @@ Created on Sat Oct 12 13:28:49 2019
 @version: 0.1a
 """
 
-import HerdMemberCreation
+import HerdMemberCreation, os
 
 herd = []
 sort_Herd = []
 iterations = 1
 average_Fitness = 0.0
-best_Position = ()
-herdSize = 0
-valid = ()
+herd_Size = 0
 BNF_Path = ""
 
 codon_Min = 0
@@ -25,22 +23,47 @@ max_Iterations = 0
 
 
 def initialiseHerd(size, iterations , min_Codon, max_Codon, size_Codon, BNF):
-    herdSize = size
+    herd_Size = size
     max_Iterations = iterations
     codon_Min = min_Codon
     codon_Max = max_Codon
     codon_Size = size_Codon
     BNF_Path = BNF
-    for i in range(0, herdSize):
+    for i in range(0, herd_Size):
         print("Running")
         herd.append(HerdMemberCreation.HerdMember(i, codon_Min, codon_Max, codon_Size, BNF_Path))
+
+def initialiseHerdFile(filePath):
+    parameters = [0]*6
+    print(os.getcwd())
+    try:
+        f = open(filePath)
+        for i in range(0, 5):
+            line = f.readline(i)
+            print("line = " + line + "\n")
+            parameters[i] = ''.join(line.partition("#"))
+            print("p[i] = " + parameters[i])
+            if i < 5:
+                p = parameters[i]
+                parameters[i] = int(p)
+        print(parameters)
+        initialiseHerd(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4], parameters[5])
+    except IOError:
+        print("The file at " + filePath + " Could not be opened, IOError.")
+
+
+
+
+def getAvgFitness():
+    avg_Fitness = 0
+    for i in range(herd_Size):
+
+        avg_Fitness = avg_Fitness + herd[i].getFitness()
 
 def evaluateHerd():
     itera = 0
     while itera < max_Iterations:
         itera = itera + 1
-
-
 
 def getCodonMax():
     return codon_Max
@@ -55,10 +78,10 @@ def getBNF_Path():
     return BNF_Path
 
 def getSize():
-    return herdSize
+    return herd_Size
 
 
-initialiseHerd(4,1,20,20,8,"SFT.BNF")
+initialiseHerdFile('parameters\\initialiseHerd.txt')
 
 print(herd[0].bitString)
 print(herd[0].phenotype)
